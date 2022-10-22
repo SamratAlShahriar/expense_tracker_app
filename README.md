b1d85a7a20476da266b2092e848b9916ab4fa829
samratalshahriar <samratalshahriar@gmail.com>
2022-10-22 21:51:55 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# expense_tracker_app
+ expense tracker flutter app for pencilbox team project helloworld
---
+++ android/app/src/main/kotlin/pb/helloworld/expense_tracker_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package pb.helloworld.expense_tracker_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
